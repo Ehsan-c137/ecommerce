42885592633aa2a216e5a7ed27e01bd18d68cb95
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-10-06 22:51:32 +0330

    add Card

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+interface IProps {
+   name?: string;
+   price?: string | number;
+   isInStock?: boolean;
+}
+
+export default function Card({ name, price, isInStock }: IProps) {
+   return (
+      <div className="rounded-[4px] max-w-[264px] max-h-[434px] flex flex-col gap-6 py-4 px-8">
+         <div className="w-[248px] h-[312px] bg-neutral-100">
+            <Image src={"/tshirt.png"} alt="clothes" width={237} height={312} />
+         </div>
+         <div className="flex flex-col gap-3">
+            <p className="font-medium text-neutral-900">
+               {name ?? "Classic Monochrome Tees"}
+            </p>
+            <div className="flex items-center gap-2">
+               <p className="px-4 py-[2px] border border-neutral-100 rounded-full">
+                  IN STOCK
+               </p>
+               <p className="text-neutral-600">$35.00</p>
+            </div>
+         </div>
+      </div>
+   );
+}
