6307f0952479157813f271843ffbe7ee8653a565
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-10-09 16:11:49 +0330

    add X & Minus Icon

--- src/components/Icons/icons.tsx
+++ src/components/Icons/icons.tsx
@@ -347,6 +347,71 @@ export const Icons = {
          />
       </svg>
    ),
+   X: () => (
+      <svg
+         width="24"
+         height="24"
+         viewBox="0 0 24 24"
+         fill="none"
+         xmlns="http://www.w3.org/2000/svg"
+      >
+         <path
+            d="M17 7L7 17"
+            stroke="#5C5F6A"
+            stroke-width="1.7"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+         />
+         <path
+            d="M7 7L17 17"
+            stroke="#5C5F6A"
+            stroke-width="1.7"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+         />
+      </svg>
+   ),
+   Plus: () => (
+      <svg
+         width="24"
+         height="24"
+         viewBox="0 0 24 24"
+         fill="none"
+         xmlns="http://www.w3.org/2000/svg"
+      >
+         <path
+            d="M12 6V18"
+            stroke="#5C5F6A"
+            stroke-width="1.7"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+         />
+         <path
+            d="M6 12H18"
+            stroke="#5C5F6A"
+            stroke-width="1.7"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+         />
+      </svg>
+   ),
+   Minus: () => (
+      <svg
+         width="24"
+         height="24"
+         viewBox="0 0 24 24"
+         fill="none"
+         xmlns="http://www.w3.org/2000/svg"
+      >
+         <path
+            d="M5 12H17"
+            stroke="#5C5F6A"
+            stroke-width="1.7"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+         />
+      </svg>
+   ),
 };
 
 export type TIcon = keyof typeof Icons;
