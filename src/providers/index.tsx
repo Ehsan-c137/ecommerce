44b21715cd8f92257c5087f9b0b544a8d55bd9fb
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-10-13 23:38:24 +0330

    add SSR

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,9 +1,38 @@
+// In Next.js, this file would be called: app/providers.tsx
 "use client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Since QueryClientProvider relies on useContext under the hood, we have to put 'use client' on top
+import {
+   isServer,
+   QueryClient,
+   QueryClientProvider,
+} from "@tanstack/react-query";
+import { useState } from "react";
 import { Toaster } from "react-hot-toast";
 
-const queryClient = new QueryClient();
+function makeQueryClient() {
+   return new QueryClient({
+      defaultOptions: {
+         queries: {
+            staleTime: 60 * 1000,
+         },
+      },
+   });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+   if (isServer) {
+      return makeQueryClient();
+   } else {
+      if (!browserQueryClient) browserQueryClient = makeQueryClient();
+      return browserQueryClient;
+   }
+}
+
 export default function Providers({ children }: { children: React.ReactNode }) {
+   const [queryClient] = useState(getQueryClient());
    return (
       <>
          <Toaster />
