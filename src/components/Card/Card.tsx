9739d8d27317b4bf93c561bc020b1b1c57d253c1
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-10-16 23:49:41 +0330

    connect to api

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface IProps {
    name?: string;
@@ -6,23 +7,32 @@ interface IProps {
    isInStock?: boolean;
 }
 
-export default function Card({ name, price, isInStock }: IProps) {
+export default function Card({ data }) {
    return (
-      <div className="rounded-[4px]  flex flex-col gap-6">
-         <div className="w-[248px] h-[312px] bg-neutral-100">
-            <Image src={"/tshirt.png"} alt="clothes" width={237} height={312} />
+      <Link
+         href={`products/${data?.slug?.toLowerCase()}`}
+         className="rounded-[4px]  flex flex-col gap-6"
+      >
+         <div className="flex items-center justify-center w-[248px] h-[312px] bg-neutral-100">
+            <Image
+               src={data?.main_image}
+               alt={data?.name}
+               width={230}
+               unoptimized
+               objectFit="contain"
+               height={312}
+               className="object-contain"
+            />
          </div>
          <div className="flex flex-col gap-3">
-            <p className="font-medium text-neutral-900">
-               {name ?? "Classic Monochrome Tees"}
-            </p>
+            <p className="font-medium text-neutral-900">{data?.name}</p>
             <div className="flex items-center gap-2">
                <p className="px-4 py-[2px] border border-neutral-100 rounded-full">
                   IN STOCK
                </p>
-               <p className="text-neutral-600">$35.00</p>
+               <p className="text-neutral-600">${data?.price}</p>
             </div>
          </div>
-      </div>
+      </Link>
    );
 }
