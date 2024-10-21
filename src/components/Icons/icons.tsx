5426a8a293817c212fe582c06244a6db2c3dcc33
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-10-21 12:48:18 +0330

    add RedHeart Icon

--- src/components/Icons/icons.tsx
+++ src/components/Icons/icons.tsx
@@ -463,6 +463,26 @@ export const Icons = {
          />
       </svg>
    ),
+   HeartRed: () => (
+      <svg
+         width="24"
+         height="24"
+         viewBox="0 0 24 24"
+         fill="none"
+         xmlns="http://www.w3.org/2000/svg"
+      >
+         <path
+            d="M12.0048 19L4.791 12.5513C0.870448 8.68258 6.63333 1.25337 12.0048 7.26384C17.3763 1.25337 23.1141 8.70729 19.22 12.5513L12.0048 19Z"
+            stroke="#FE251B 
+"
+            fill="#FE251B 
+"
+            stroke-width="1.42857"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+         />
+      </svg>
+   ),
    Star: () => (
       <svg
          width="24"
@@ -590,6 +610,23 @@ export const Icons = {
             stroke-width="1.42857"
             stroke-linecap="round"
             stroke-linejoin="round"
+         />
+      </svg>
+   ),
+   ChevronRight: (props: React.SVGProps<SVGSVGElement>) => (
+      <svg
+         width="24"
+         height="24"
+         viewBox="0 0 24 24"
+         fill="none"
+         xmlns="http://www.w3.org/2000/svg"
+         {...props}
+      >
+         <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M14.8117 11.4673C15.0609 11.7443 15.063 12.1958 14.8163 12.4757L9.94859 18L9.04604 16.9967L13.4672 11.9793L9 7.01366L9.89321 6L14.8117 11.4673Z"
+            fill="#5C5F6A"
          />
       </svg>
    ),
