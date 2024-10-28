84e1d847a2341ad7a84d129dc826c31ace7755d8
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-10-28 20:16:12 +0330

    add min price check filter

--- src/app/(user)/products/Products.tsx
+++ src/app/(user)/products/Products.tsx
@@ -1,74 +1,61 @@
-"use client";
+"use client"
 
-import Card from "@/components/Card/Card";
-import products from "@/services/store/product/products";
-import useGetAllSearchParams from "@/utils/useGetAllSearchParams";
-import { useQuery } from "@tanstack/react-query";
-import { Categories } from "@/utils/constant";
-import { useSearchParams } from "next/navigation";
-import { useCallback, useMemo } from "react";
-import useHandleQueryParams from "@/utils/useHandleQueryParams";
-import { Icons } from "@/components/Icons/icons";
+import Card from "@/components/Card/Card"
+import products from "@/services/store/product/products"
+import useGetAllSearchParams from "@/utils/useGetAllSearchParams"
+import { useQuery } from "@tanstack/react-query"
+import { Categories } from "@/utils/constant"
+import { useSearchParams } from "next/navigation"
+import { useCallback, useMemo } from "react"
+import useHandleQueryParams from "@/utils/useHandleQueryParams"
+import { Icons } from "@/components/Icons/icons"
 
-export default function Products() {
-   const searchParams = useSearchParams();
-   const allSearchParams = useGetAllSearchParams();
-   const groupped = Object.groupBy(allSearchParams, (item) => item.name);
-   const allSearchParamsObj = {};
+export default function Products({ maxPrice, minPrice }) {
+   const searchParams = useSearchParams()
+   const allSearchParams = useGetAllSearchParams()
+   const groupped = Object.groupBy(allSearchParams, (item) => item.name)
+   const allSearchParamsObj = {}
    Object.entries(groupped).forEach(([key, value]) => {
-      allSearchParamsObj[key] = value.map((item) => item.value);
-   });
+      allSearchParamsObj[key] = value.map((item) => item.value)
+   })
 
-   const handleQueryParams = useHandleQueryParams();
+   const handleQueryParams = useHandleQueryParams()
 
    const checkParams = (array: string[], items: []) => {
-      return items?.some((item) => array?.includes(item));
-   };
+      return items?.some((item) => array?.includes(item))
+   }
 
    const { data, isLoading, isError, error } = useQuery({
       queryKey: ["allproducts"],
       queryFn: () => products(2, 1),
-   });
-
-   const minPrice = data?.reduce(
-      (min, item) => Math.min(min, item.price),
-      data[0]?.price
-   );
-
-   const maxPrice = data?.reduce(
-      (max, item) => Math.max(max, item.price),
-      data[0]?.price
-   );
+   })
 
    const filterProduct = useCallback(
       (data) => {
          return data?.filter((product) => {
-            const productColors = product.options?.colors;
-            const productSizes = product.options?.sizes;
-            const productCategory = product.category;
+            const productColors = product.options?.colors
+            const productSizes = product.options?.sizes
+            const productCategory = product.category
 
             const isThereColor = checkParams(
                searchParams.getAll("color"),
                productColors
-            );
-
-            const minPrice = searchParams.get("min-price");
-            const maxPrice = searchParams.get("max-price");
+            )
 
             const isThereSize = checkParams(
                searchParams.getAll("size"),
                productSizes
-            );
+            )
 
             if (allSearchParams.length > 0) {
                if (isThereColor) {
-                  return product;
+                  return product
                }
                if (isThereSize) {
-                  return product;
+                  return product
                }
-               if (product.price > minPrice && product.price <= maxPrice) {
-                  return product;
+               if (product.price >= minPrice && product.price <= maxPrice) {
+                  return product
                }
 
                if (
@@ -76,28 +63,36 @@ export default function Products() {
                      .getAll("category")
                      ?.includes(Categories[productCategory])
                )
-                  return product;
+                  return product
             } else {
-               return product;
+               return product
             }
-         });
+         })
       },
       [allSearchParams, searchParams]
-   );
+   )
 
    const filteredData = useMemo(
       () => data && filterProduct(data),
       [data, filterProduct]
-   );
+   )
 
    return (
       <>
-         {allSearchParams.length > 0 && (
+         {allSearchParams.filter(
+            (item) => item.name !== "max price" && item.name !== "min price"
+         ).length > 0 && (
             <p className="text-neutral-900 font-medium">Applied Filters:</p>
          )}
 
          <div className="flex flex-wrap gap-3">
             {Object.entries(allSearchParamsObj).map(([key, value]) => {
+               if (key === "max price" && Number(value[0]) === maxPrice) {
+                  return
+               }
+               if (key === "min price" && Number(value[0]) === minPrice) {
+                  return
+               }
                return (
                   <div key={key} className="flex items-center gap-2">
                      <p className="font-medium">
@@ -112,16 +107,16 @@ export default function Products() {
                               {item[0]?.toUpperCase() + item?.slice(1)}
                               <div
                                  onClick={() => {
-                                    handleQueryParams(key, item);
+                                    handleQueryParams(key, item)
                                  }}
                               >
                                  <Icons.X />
                               </div>
                            </button>
-                        );
+                        )
                      })}
                   </div>
-               );
+               )
             })}
          </div>
          <div className="text-neutral-500 justify-between flex items-center w-full py-4">
@@ -168,5 +163,5 @@ export default function Products() {
             ))}
          </div>
       </>
-   );
+   )
 }
