77aaff14099276ef2c7b3b334ba11fa1db1dec70
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-11-06 23:40:04 +0330

    add colors

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -2,3 +2,12 @@ export enum Categories {
    "tshirt" = 1,
    "hoodie" = 2,
 }
+
+export const Colors = {
+   blue: "#A3BEF8",
+   yellow: "#FFD58A",
+   green: "#83B18B",
+   red: "#F8C5B9",
+   white: "#F6F6F6",
+   black: "#000000",
+}
