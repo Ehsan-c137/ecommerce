ca5cb35c7ba37de80c26bdf8eed952894b0c61d7
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-11-06 23:36:40 +0330

    add cart api

--- src/app/(user)/cart/page.tsx
+++ src/app/(user)/cart/page.tsx
@@ -1,8 +1,44 @@
-import Breadcrumb from "@/components/Breadcrumb/Breadcrumb";
-import Item from "./Item";
-import Link from "next/link";
+"use client"
+
+import Breadcrumb from "@/components/Breadcrumb/Breadcrumb"
+import Item from "./Item"
+import Link from "next/link"
+import { useQuery, useMutation } from "@tanstack/react-query"
+import { getCart } from "@/services/store/cart/Cart"
+import toast from "react-hot-toast"
+
+import { IPutCart, putCart } from "@/services/store/cart/Cart"
 
 export default function Cart() {
+   const { data: cart } = useQuery({
+      queryKey: ["cart"],
+      queryFn: () => getCart(),
+   })
+
+   const mutation = useMutation({
+      mutationFn: (data: IPutCart[]) => putCart(data),
+      onError: (error) => {
+         console.log("error", error)
+         toast.error("something went wrong")
+      },
+   })
+
+   const handleCart = (id, count, colors, sizes) => {
+      const newDataCart = cart
+      const findIndex = newDataCart?.data?.findIndex(
+         (item) =>
+            item.data.id == id && item.colors == colors && item.sizes == sizes
+      )
+
+      newDataCart.data[findIndex].count = count
+
+      mutation.mutate(newDataCart.data)
+   }
+
+   const total = cart?.data?.reduce((acc, item) => {
+      return item.count * Number(item.data?.price) + acc
+   }, 0)
+
    return (
       <>
          <div className="bg-white-100 h-40 flex items-center ">
@@ -17,8 +53,14 @@ export default function Cart() {
                   Your cart
                </h5>
                <div className="flex flex-col pt-12 gap-10">
-                  <Item />
-                  <Item />
+                  {cart?.data?.length === 0 && "Your cart is empty"}
+                  {cart?.data?.map((item) => (
+                     <Item
+                        key={item.colors + item.sizes}
+                        item={item}
+                        handleCart={handleCart}
+                     />
+                  ))}
                </div>
             </div>
             <div className="flex flex-col border border-neutral-100 rounded-md px-6 py-8 w-[300px] max-w-[340px]">
@@ -28,7 +70,7 @@ export default function Cart() {
                <div className="flex flex-col font-medium gap-4">
                   <div className="flex items-center justify-between">
                      <p className="text-neutral-500">Subtotal</p>
-                     <p className="text-neutral-900 text-nowrap">$ 90.00</p>
+                     <p className="text-neutral-900 text-nowrap">$ {total}</p>
                   </div>
                   <div className="flex justify-between items-start">
                      <p className="text-neutral-500">Shipping: </p>
@@ -43,7 +85,7 @@ export default function Cart() {
                <div className="flex flex-col gap-6">
                   <div className="flex justify-between items-start">
                      <p className="text-neutral-500">Total</p>
-                     <p className="text-neutral-900">$ 100.00</p>
+                     <p className="text-neutral-900">$ {total + 3}</p>
                   </div>
                   <button className="btn text-center justify-center mt-">
                      Checkout
@@ -60,5 +102,5 @@ export default function Cart() {
             </div>
          </div>
       </>
-   );
+   )
 }
