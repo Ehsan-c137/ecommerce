80f45793c71446f1e885af9402091d5221438173
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-11-18 19:34:11 +0330

    add padding

--- src/app/(user)/products/page.tsx
+++ src/app/(user)/products/page.tsx
@@ -1,17 +1,12 @@
-import Filters from "./Filters";
-import Breadcrumb from "@/components/Breadcrumb/Breadcrumb";
+import Filters from "./Filters"
+import Breadcrumb from "@/components/UI/Breadcrumb"
 
 export default function Page() {
    return (
       <>
-         <div className="h-16 w-full flex items-center bg-white-100">
-            <div className="container mx-auto flex items-center">
-               <Breadcrumb />
-            </div>
-         </div>
-         <div className="flex gap-6 container mx-auto mt-8">
+         <div className="flex gap-6 container mx-auto mt-4 px-4">
             <Filters />
          </div>
       </>
-   );
+   )
 }
