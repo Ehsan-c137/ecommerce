09796cf856442f7cd5f2573079ab4f04319d73da
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-11-18 19:35:07 +0330

    add profile data

--- src/app/(user)/profile/page.tsx
+++ src/app/(user)/profile/page.tsx
@@ -1,21 +1,21 @@
-"use client";
+"use client"
 
-import Link from "next/link";
-import { Icons } from "../Icons/icons";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import logout from "@/services/user/logout";
-import getProfile from "@/services/user/change_info";
+import Link from "next/link"
+import { Icons } from "../Icons/icons"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import logout from "@/services/user/logout"
+import getUserInfo from "@/services/user/change_info"
 
 export default function Profile() {
    const { data, status, isSuccess, isLoading } = useQuery({
       queryKey: ["profile"],
-      queryFn: () => getProfile(),
-   });
+      queryFn: () => getUserInfo(),
+   })
    return (
       <div className="container mx-auto">
          {isLoading && "is Loading"}
          <p>{data?.username}</p>
          <p>{data?.email}</p>
       </div>
-   );
+   )
 }
