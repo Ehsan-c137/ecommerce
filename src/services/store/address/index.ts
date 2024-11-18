206e83c3da0e045319544b064ff30980689d2d40
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-11-18 19:41:57 +0330

    add auth

--- src/services/store/address/index.ts
+++ src/services/store/address/index.ts
@@ -1,17 +1,34 @@
+"use server"
+
 import api from "@/services/index"
+import { cookies } from "next/headers"
 
 export async function getAllAddress() {
    try {
-      const response = await api.get("/store/address")
-      return response.data
+      const { data } = await api.get("/store/address/", {
+         headers: {
+            Authorization: `Token ${cookies().get("session")?.value}`,
+         },
+      })
+
+      return data
    } catch (error) {
       return error
    }
 }
 
-export async function createAddress(data) {
+export async function createAddress(data: {
+   name: string
+   phone: string
+   address: string
+   coordinate?: string
+}) {
    try {
-      const response = await api.post("/store/address", data)
+      const response = await api.post("/store/address/", data, {
+         headers: {
+            Authorization: `Token ${cookies().get("session")?.value}`,
+         },
+      })
       return response.data
    } catch (error) {
       return error
@@ -20,7 +37,11 @@ export async function createAddress(data) {
 
 export async function editAddress(addressId: string) {
    try {
-      const response = await api.put(`/store/address/${addressId}`)
+      const response = await api.put(`/store/address/${addressId}`, {
+         headers: {
+            Authorization: `Token ${cookies().get("session")?.value}`,
+         },
+      })
       return response.data
    } catch (error) {
       return error
@@ -29,7 +50,11 @@ export async function editAddress(addressId: string) {
 
 export async function deleteAddress(addressId: string) {
    try {
-      const response = await api.delete(`/store/address/${addressId}`)
+      const response = await api.delete(`/store/address/${addressId}`, {
+         headers: {
+            Authorization: `Token ${cookies().get("session")?.value}`,
+         },
+      })
       return response.data
    } catch (error) {
       return error
