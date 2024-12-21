9d0e34e514b102d0517e0029d2b2195a03e1f0b2
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-12-21 22:28:25 +0330

    add category and description section

--- src/app/admin/products/AddProduct.tsx
+++ src/app/admin/products/AddProduct.tsx
@@ -7,6 +7,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { useRouter } from "next/navigation";
+import { Icons } from "@/components/Icons/icons";
 
 const SigninSchema = z.object({
    title: z
@@ -21,6 +22,7 @@ const SigninSchema = z.object({
       .min(1, { message: "Available Quantity is required" }),
    stock_status: z.string().min(1, { message: "stock status is required" }),
    available_quantity: z.number().min(0, { message: "quantity is required" }),
+   description: z.string(),
 });
 
 type TSigninSchema = z.infer<typeof SigninSchema>;
@@ -34,6 +36,10 @@ export default function AddProduct() {
       resolver: zodResolver(SigninSchema),
       mode: "onChange",
    });
+
+   const colors = ["yellow", "red", "blue"];
+
+   const sizes = ["s", "m", "xl", "xxl", "xxl"];
 
    const queryClient = useQueryClient();
    const router = useRouter();
@@ -65,126 +71,229 @@ export default function AddProduct() {
    };
 
    return (
-      <div className="container mx-auto w-full flex flex-col gap-6 items-center justify-center min-h-[80vh]">
-         <form action="" className="flex flex-col items-start gap-4">
+      <div className="w-full flex flex-col gap-6 justify-center">
+         <h4 className="font-medium text-neutral-900 py-6 border-b border-white-200 px-10">
+            Add Product
+         </h4>
+         <form action="" className="flex gap-20 px-6 lg:px-10">
             <div className="flex flex-col">
-               <label
-                  htmlFor="title"
-                  className="text-neutral-600 font-medium text-sm"
+               <div className="flex flex-col">
+                  <label
+                     htmlFor="title"
+                     className="text-neutral-600 font-medium text-sm"
+                  >
+                     Title
+                  </label>
+                  <input
+                     autoComplete="true"
+                     {...register("title")}
+                     type="text"
+                     id="title"
+                     className="border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
+                  />
+                  <p className="text-red-r500 mt-2 text-wrap">
+                     {errors.title?.message}
+                  </p>
+               </div>
+               <div className="flex flex-col">
+                  <label
+                     htmlFor="price"
+                     className="text-neutral-600 font-medium text-sm"
+                  >
+                     Price
+                  </label>
+                  <input
+                     autoComplete="true"
+                     {...register("price")}
+                     type="number"
+                     required
+                     id="price"
+                     className="border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
+                  />
+                  <p className="text-red-r500 mt-2 text-wrap">
+                     {errors.price?.message}
+                  </p>
+               </div>
+               <div className="flex flex-col">
+                  <label
+                     htmlFor="price"
+                     className="text-neutral-600 font-medium text-sm"
+                  >
+                     Category
+                  </label>
+                  <input
+                     autoComplete="true"
+                     {...register("category")}
+                     type="number"
+                     required
+                     id="category"
+                     className="border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
+                  />
+                  <p className="text-red-r500 mt-2 text-wrap">
+                     {errors.category?.message}
+                  </p>
+               </div>
+               <div className="flex flex-col">
+                  <label
+                     htmlFor="title"
+                     className="text-neutral-600 font-medium text-sm"
+                  >
+                     Slug
+                  </label>
+                  <input
+                     autoComplete="true"
+                     {...register("slug")}
+                     type="text"
+                     id="slug"
+                     className="border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
+                  />
+                  <p className="text-red-r500 mt-2 text-wrap">
+                     {errors.slug?.message}
+                  </p>
+               </div>
+               <div className="flex flex-col">
+                  <label
+                     htmlFor="title"
+                     className="text-neutral-600 font-medium text-sm"
+                  >
+                     SKU
+                  </label>
+                  <input
+                     autoComplete="true"
+                     {...register("sku")}
+                     type="text"
+                     id="sku"
+                     className="border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
+                  />
+                  <p className="text-red-r500 mt-2 text-wrap">
+                     {errors.sku?.message}
+                  </p>
+               </div>
+               <button
+                  className="btn w-full flex items-center justify-center gap-4 mt-4"
+                  disabled={!isValid || signinMutation.isPending}
+                  onClick={handleSignin}
                >
-                  Username
-               </label>
-               <input
-                  autoComplete="true"
-                  {...register("title")}
-                  type="text"
-                  id="title"
-                  className="border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
-               />
-               <p className="text-red-r500 mt-2 text-wrap">
-                  {errors.title?.message}
-               </p>
+                  Save products
+                  {signinMutation.isPending && <span className="loader"></span>}
+               </button>
             </div>
-            <div className="flex flex-col">
-               <label
-                  htmlFor="price"
-                  className="text-neutral-600 font-medium text-sm"
-               >
-                  Password
-               </label>
-               <input
-                  autoComplete="true"
-                  {...register("price")}
-                  type="number"
-                  required
-                  id="price"
-                  className="border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
-               />
-               <p className="text-red-r500 mt-2 text-wrap">
-                  {errors.price?.message}
-               </p>
-            </div>
-            <div className="flex flex-col">
-               <label
-                  htmlFor="title"
-                  className="text-neutral-600 font-medium text-sm"
-               >
-                  Slug
-               </label>
-               <input
-                  autoComplete="true"
-                  {...register("slug")}
-                  type="text"
-                  id="slug"
-                  className="border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
-               />
-               <p className="text-red-r500 mt-2 text-wrap">
-                  {errors.slug?.message}
-               </p>
-            </div>
-            <div className="flex flex-col">
-               <label
-                  htmlFor="title"
-                  className="text-neutral-600 font-medium text-sm"
-               >
-                  SKU
-               </label>
-               <input
-                  autoComplete="true"
-                  {...register("sku")}
-                  type="text"
-                  id="sku"
-                  className="border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
-               />
-               <p className="text-red-r500 mt-2 text-wrap">
-                  {errors.sku?.message}
-               </p>
-            </div>
-            <div className="flex flex-col">
-               <label
-                  htmlFor="title"
-                  className="text-neutral-600 font-medium text-sm"
-               >
-                  Stock status
-               </label>
-               <input
-                  autoComplete="true"
-                  {...register("stock_status")}
-                  type="text"
-                  id="sku"
-                  className="border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
-               />
-               <p className="text-red-r500 mt-2 text-wrap">
-                  {errors.sku?.message}
-               </p>
-            </div>
-            <div className="flex flex-col">
-               <label
-                  htmlFor="title"
-                  className="text-neutral-600 font-medium text-sm"
-               >
-                  Available quantity
-               </label>
-               <input
-                  autoComplete="true"
-                  {...register("available_quantity")}
-                  type="text"
-                  id="available_quantity"
-                  className="border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
-               />
-               <p className="text-red-r500 mt-2 text-wrap">
-                  {errors.available_quantity?.message}
-               </p>
-            </div>
+            <div>
+               <div className="flex flex-col">
+                  <label
+                     htmlFor="title"
+                     className="text-neutral-600 font-medium text-sm"
+                  >
+                     Stock status
+                  </label>
+                  <input
+                     autoComplete="true"
+                     {...register("stock_status")}
+                     type="text"
+                     id="sku"
+                     className="border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
+                  />
+                  <p className="text-red-r500 mt-2 text-wrap">
+                     {errors.sku?.message}
+                  </p>
+               </div>
+               <div className="flex flex-col">
+                  <label
+                     htmlFor="title"
+                     className="text-neutral-600 font-medium text-sm"
+                  >
+                     Available quantity
+                  </label>
+                  <input
+                     autoComplete="true"
+                     {...register("available_quantity")}
+                     type="text"
+                     id="available_quantity"
+                     className="border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
+                  />
+                  <p className="text-red-r500 mt-2 text-wrap">
+                     {errors.available_quantity?.message}
+                  </p>
+               </div>
+               <div className="flex flex-col">
+                  <label
+                     htmlFor="description"
+                     className="text-neutral-600 font-medium text-sm"
+                  >
+                     Description
+                  </label>
+                  <input
+                     autoComplete="true"
+                     {...register("description")}
+                     type="text"
+                     id="description"
+                     className="border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
+                  />
+                  <p className="text-red-r500 mt-2 text-wrap">
+                     {errors.description?.message}
+                  </p>
+               </div>
+               <div className="flex flex-col">
+                  <label
+                     htmlFor="image"
+                     className="text-neutral-600 font-medium text-sm"
+                  >
+                     Images
+                  </label>
+                  <label
+                     htmlFor="image"
+                     className="cursor-pointer flex items-center gap-2 text-neutral-600 font-medium text-sm border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
+                  >
+                     <Icons.Upload /> Choose product images
+                  </label>
+                  <input
+                     autoComplete="true"
+                     {...register("image")}
+                     type="file"
+                     id="image"
+                     placeholder="Choose product images"
+                     name="image"
+                     accept="image/png, image/jpeg"
+                     className="hidden"
+                     multiple
+                  />
 
-            <button
-               className="btn w-full flex items-center justify-center gap-4"
-               disabled={!isValid || signinMutation.isPending}
-               onClick={handleSignin}
-            >
-               Login
-               {signinMutation.isPending && <span className="loader"></span>}
-            </button>
+                  <p className="text-red-r500 mt-2 text-wrap">
+                     {errors.description?.message}
+                  </p>
+               </div>
+               <div className="flex gap-2">
+                  <div>
+                     <input
+                        type="radio"
+                        id="addproduct_red"
+                        name="colors"
+                        value="red"
+                     />
+                     <label htmlFor="addproduct_red">red</label>
+                  </div>
+
+                  <div>
+                     <input
+                        type="radio"
+                        id="addproduct_blue"
+                        name="colors"
+                        value="blue"
+                     />
+                     <label htmlFor="addproduct_blue">blue</label>
+                  </div>
+
+                  <div>
+                     <input
+                        type="radio"
+                        id="addproduct_yellow"
+                        name="colors"
+                        value="yellow"
+                     />
+                     <label htmlFor="addproduct_yellow">yellow</label>
+                  </div>
+               </div>
+            </div>
          </form>
          <p className="text-neutral-500">
             Don&apos;t have an account? <Link href="/signup"> Sign up </Link>
