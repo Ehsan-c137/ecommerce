8e39b0f8cbe94716edf15161d792067538aa1f71
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-12-21 22:01:11 +0330

    change layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,13 @@
-import Link from "next/link";
+import Link from "next/link"
 
 export default function NotFound() {
    return (
-      <div className="w-[100vw] h-[100vh] flex flex-col items-center justify-center">
-         <h1>404</h1>
-         <Link href={"/"}>Go To landing</Link>
+      <div className="w-[100vw] h-[100vh] flex items-center gap-4 justify-center">
+         <p className="font-bold ">404</p>
+         <p>|</p>
+         <Link href={"/"} className="hover:underline transition">
+            Go to home
+         </Link>
       </div>
-   );
+   )
 }
