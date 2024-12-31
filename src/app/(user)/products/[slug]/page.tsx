43f4c912d289a05d959902d89487151a7be13db8
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-01 00:05:06 +0330

    fix genreate metadata

--- src/app/(user)/products/[slug]/page.tsx
+++ src/app/(user)/products/[slug]/page.tsx
@@ -23,12 +23,12 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
    const id = (await params).slug
 
    // fetch data
-   const product = await fetch(`${BASE_URL}/store/product/${id}`).then((res) =>
-      res.json()
+   const product = await fetch(`${BASE_URL}/store/product/slug/${id}`).then(
+      (res) => res.json()
    )
 
    return {
-      title: product.title,
+      title: product?.name,
    }
 }
 
