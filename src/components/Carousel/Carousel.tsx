c823ede55462c3ccd306cb38d1ce7d18ba4a38f6
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-01 00:05:33 +0330

    initial commit

---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import { useState } from "react"
+import "./Carousel.css"
+import Image from "next/image"
+import { Icons } from "../Icons/icons"
+
+const CarouselIndicators = ({ images, activeIndex, onClick }) => {
+   return (
+      <div className="carousel__indicators">
+         {images.map((_, index) => (
+            <span
+               key={index}
+               className={`carousel__indicator ${
+                  index === activeIndex ? "active" : ""
+               }`}
+               onClick={() => onClick(index)}
+            />
+         ))}
+      </div>
+   )
+}
+const Carousel = ({ images, interval = 3000 }) => {
+   const [activeIndex, setActiveIndex] = useState(0)
+   const nextSlide = () => {
+      setActiveIndex((prevIndex) =>
+         prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      )
+   }
+   const prevSlide = () => {
+      setActiveIndex((prevIndex) =>
+         prevIndex === 0 ? images.length - 1 : prevIndex - 1
+      )
+   }
+
+   return (
+      <div className="carousel flex items-center max-w-[600px]">
+         <button
+            onClick={prevSlide}
+            className="carousel__btn carousel__btn--prev"
+         >
+            <Icons.ArrowLeft />
+         </button>
+         <div className="overflow-hidden">
+            <div
+               style={{
+                  transform: `translateX(-${activeIndex * 100}%)`,
+                  transitionDuration: "400ms",
+               }}
+               className=" w-full items-center flex [transition-timing-function:cubic-bezier(0.61,1,0.88,1)]"
+            >
+               {images.map((image: string, index: number) => {
+                  return (
+                     <Image
+                        key={image}
+                        src={image}
+                        alt={`Slide ${index}`}
+                        className={`carousel__img ${
+                           index === activeIndex ? "active" : ""
+                        }`}
+                        sizes="100vw"
+                        style={{
+                           width: "100%",
+                           height: "100%",
+                           objectFit: "cover",
+                        }}
+                        width={0}
+                        height={0}
+                     />
+                  )
+               })}
+            </div>
+         </div>
+         <button
+            onClick={nextSlide}
+            className="carousel__btn carousel__btn--next"
+         >
+            <Icons.ArrowRight />
+         </button>
+      </div>
+   )
+}
+export default Carousel
