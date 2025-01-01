acffe82b7fa9f9bfd442a9851b158c590cbc5028
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-01 20:35:59 +0330

    fix: add checkout cart when cart is't empty

--- src/app/(user)/cart/layout.tsx
+++ src/app/(user)/cart/layout.tsx
@@ -23,80 +23,84 @@ export default function CartLayout({ children }: { children: ReactNode }) {
       <>
          <div className="container flex flex-col md:flex-row gap-12 justify-between pt-10 px-4 lg:px-0">
             <div>{children}</div>
-            <div className="flex flex-col border border-neutral-100 rounded-md px-6 py-8 w-[300px] max-w-[340px]">
-               <h5 className="text-neutral-900 font-normal mb-10">
-                  {pathname === "/cart" && "Order Summary"}
-                  {pathname === "/cart/checkout" && "Your Order"}
-               </h5>
-               {pathname === "/cart/checkout" && (
-                  <div>
-                     <button
-                        onClick={() => {
-                           router.push("/cart")
-                        }}
-                        className="btn-outline"
-                     >
-                        Edit Cart
-                     </button>
-                  </div>
-               )}
-               <div className="flex flex-col font-medium gap-4">
-                  <div className="flex items-center justify-between">
-                     <p className="text-neutral-500">Subtotal</p>
-
-                     {isLoading ? (
-                        <div
-                           className="w-[34px] h-[20px] animate-pulse bg-white-200 rounded-md"
-                           role="status"
+            {cart?.data.length > 0 && (
+               <div className="flex flex-col border border-neutral-100 rounded-md px-6 py-8 w-[300px] max-w-[340px]">
+                  <h5 className="text-neutral-900 font-normal mb-10">
+                     {pathname === "/cart" && "Order Summary"}
+                     {pathname === "/cart/checkout" && "Your Order"}
+                  </h5>
+                  {pathname === "/cart/checkout" && (
+                     <div>
+                        <button
+                           onClick={() => {
+                              router.push("/cart")
+                           }}
+                           className="btn-outline"
                         >
-                           <div className="sr-only">loading</div>
-                        </div>
-                     ) : (
-                        <p className="text-neutral-900 text-nowrap">${total}</p>
-                     )}
-                  </div>
-                  <div className="flex justify-between items-start">
-                     <p className="text-neutral-500">Shipping: </p>
-                     <p className="text-neutral-900">Free</p>
-                  </div>
-                  <div className="flex justify-between items-start">
-                     <p className="text-neutral-500">Tax</p>
-                     <p className="text-neutral-900">$ 3.00</p>
-                  </div>
-               </div>
-               <div className="w-full h-[1px] bg-neutral-100 my-6"></div>
-               <div className="flex flex-col gap-6">
-                  <div className="flex justify-between items-start">
-                     <p className="text-neutral-500">Total</p>
+                           Edit Cart
+                        </button>
+                     </div>
+                  )}
+                  <div className="flex flex-col font-medium gap-4">
+                     <div className="flex items-center justify-between">
+                        <p className="text-neutral-500">Subtotal</p>
 
-                     {isLoading ? (
-                        <div
-                           className="w-[34px] h-[20px] animate-pulse bg-white-200 rounded-md"
-                           role="status"
-                        >
-                           <div className="sr-only">loading</div>
-                        </div>
-                     ) : (
-                        <p className="text-neutral-900">${total + 3}</p>
-                     )}
+                        {isLoading ? (
+                           <div
+                              className="w-[34px] h-[20px] animate-pulse bg-white-200 rounded-md"
+                              role="status"
+                           >
+                              <div className="sr-only">loading</div>
+                           </div>
+                        ) : (
+                           <p className="text-neutral-900 text-nowrap">
+                              ${total}
+                           </p>
+                        )}
+                     </div>
+                     <div className="flex justify-between items-start">
+                        <p className="text-neutral-500">Shipping: </p>
+                        <p className="text-neutral-900">Free</p>
+                     </div>
+                     <div className="flex justify-between items-start">
+                        <p className="text-neutral-500">Tax</p>
+                        <p className="text-neutral-900">$ 3.00</p>
+                     </div>
                   </div>
-                  <Link
-                     href={"/cart/checkout"}
-                     className="btn text-center justify-center mt-"
-                  >
-                     {pathname === "/cart" && "Checkout"}
-                     {pathname === "/cart/checkout" && "Place Order"}
-                  </Link>
-                  <div className="flex justify-center">
+                  <div className="w-full h-[1px] bg-neutral-100 my-6"></div>
+                  <div className="flex flex-col gap-6">
+                     <div className="flex justify-between items-start">
+                        <p className="text-neutral-500">Total</p>
+
+                        {isLoading ? (
+                           <div
+                              className="w-[34px] h-[20px] animate-pulse bg-white-200 rounded-md"
+                              role="status"
+                           >
+                              <div className="sr-only">loading</div>
+                           </div>
+                        ) : (
+                           <p className="text-neutral-900">${total + 3}</p>
+                        )}
+                     </div>
                      <Link
-                        href={"/products"}
-                        className="text-neutral-900 text-xs border-b text-center  border-neutral-900 font-medium"
+                        href={"/cart/checkout"}
+                        className="btn text-center justify-center mt-"
                      >
-                        Continue Shopping
+                        {pathname === "/cart" && "Checkout"}
+                        {pathname === "/cart/checkout" && "Place Order"}
                      </Link>
+                     <div className="flex justify-center">
+                        <Link
+                           href={"/products"}
+                           className="text-neutral-900 text-xs border-b text-center  border-neutral-900 font-medium"
+                        >
+                           Continue Shopping
+                        </Link>
+                     </div>
                   </div>
                </div>
-            </div>
+            )}
          </div>
       </>
    )
