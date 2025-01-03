030c31aa5668393d673eb0540d581a580bd91c72
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-03 22:39:47 +0330

    delete extra fields

--- src/app/(user)/profile/account-detail/page.tsx
+++ src/app/(user)/profile/account-detail/page.tsx
@@ -24,7 +24,7 @@ const AddressSchema = z.object({
 type TAddressSchema = z.infer<typeof AddressSchema>
 
 export default function UserInfo() {
-   const { data, status, isSuccess, isLoading } = useQuery({
+   const { data } = useQuery({
       queryKey: ["profile"],
       queryFn: () => getUserInfo(),
    })
@@ -33,8 +33,8 @@ export default function UserInfo() {
 
    const {
       register,
-      getValues,
-      formState: { errors, isValid },
+
+      formState: { errors },
    } = useForm<TAddressSchema>({
       resolver: zodResolver(AddressSchema),
       mode: "onChange",
