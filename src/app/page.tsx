20b766f1dd0d7c982320817702cca526038f5324
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-08 14:32:59 +0330

    change design of landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,242 +1,153 @@
 import { Icons } from "@/components/Icons/icons"
+import { Bodoni_Moda } from "next/font/google"
 import Image from "next/image"
 import Link from "next/link"
 import checkLoggedin from "@/services/user/check_loggedin"
-import Profile from "@/components/Header/Profile"
-import Carousel from "@/components/Carousel/Carousel"
 import Footer from "@/components/Footer/Footer"
+import MobileNav from "@/components/Header/MobileNav"
+import Product from "@/components/UI/Product"
+import Carousel from "@/components/Carousel/Carousel"
+
+const bodoniModa = Bodoni_Moda({
+   subsets: ["latin"],
+   display: "swap",
+   style: "italic",
+})
 
 export default async function Home() {
    const isLogged = await checkLoggedin()
+
    return (
-      <main className="flex flex-col mx-auto items-center sm:items-start bg-[#F3F3F3]">
-         <div className="w-full flex items-center justify-center text-center h-[44px] bg-[#FC7A1D] font-bold uppercase">
-            Summer sale up to 50% off
-         </div>
-         <section className="w-full items-end relative">
-            <header className="absolute w-full h-21 top-0 z-10 bg-transparent text-white-100 px-4">
-               <div className="flex items-center justify-between h-[84px] 3xl:container mx-auto">
-                  <Link href={"/"} className="flex items-center gap-4">
-                     <Icons.Logo />
-                  </Link>
-                  <nav>
-                     <ul className="hidden lg:flex justify-between gap-8 text-neutral-500">
-                        <li className="hover:text-neutral-900 transition-colors">
-                           <Link href={"/"}>Home</Link>
-                        </li>
-                        <li className="hover:text-neutral-900 transition-colors">
-                           <Link href={"/products"}>Products</Link>
-                        </li>
-                        <li className="hover:text-neutral-900 transition-colors">
-                           <Link href={"/about"}>About</Link>
-                        </li>
-                        <li>
-                           <Link
-                              className="hover:text-neutral-900 transition-colors"
-                              href={"/contact"}
-                           >
-                              Contact
-                           </Link>
-                        </li>
-                     </ul>
-                  </nav>
-                  <div className="flex items-center gap-8 justify-between">
-                     {isLogged ? (
-                        <Profile />
-                     ) : (
-                        <Link className="btn" href={"/login"}>
-                           Login
-                        </Link>
-                     )}
+      <div className="background">
+         <MobileNav />
+         <main className="flex flex-col mx-auto items-center sm:items-start">
+            <section className="w-full items-end relative h-[600px]">
+               <Image
+                  src="/mobile/hero-section.jpg"
+                  alt="spring season"
+                  priority={true}
+                  width={0}
+                  height={600}
+                  sizes="100vw"
+                  unoptimized={true}
+                  style={{
+                     width: "100%",
+                     height: "600px",
+                     objectFit: "contain",
+                     objectPosition: "top",
+                  }}
+               />
+               <p
+                  className={`font-bold italic text-body absolute left-1/2 -translate-x-1/2 text-nowrap bottom-[40%] ${bodoniModa.className} uppercase text-[30px] bg-blend-multiply opacity-70 leading-tight`}
+               >
+                  <span className="ml-5">Fashion </span>
+                  <br /> & Accessories
+               </p>
+               <Link
+                  href={"/products"}
+                  className="absolute rounded-[30px] px-[30px] py-2 uppercase bg-opacity-70 bg-body bottom-[60px] text-nowrap left-[50%] text-offWhite transform -translate-x-1/2"
+               >
+                  Explore Collection
+               </Link>
+            </section>
+            <section className="flex flex-col gap-4 justify-around text-center items-center w-full py-5">
+               <p className="text-titleActive uppercase">new Arrival</p>
+               <Icons.Border />
+               <div className="w-full flex items-center justify-around px-2">
+                  <button>All</button>
+                  <button>Apparel</button>
+                  <button>Dress</button>
+                  <button>Tshirt</button>
+                  <button>Bag</button>
+               </div>
+               <div className="flex flex-wrap justify-center gap-4">
+                  <Product />
+                  <Product />
+                  <Product />
+                  <Product />
+               </div>
+               <div className="flex items-center gap-2 py-4">
+                  <p className="text-titleActive body-1">Explore More</p>
+                  <Icons.ArrowRight />
+               </div>
+            </section>
+            <section className="grid grid-cols-1 items-center justify-items-center md:grid-cols-2 container mx-auto my-4">
+               <Icons.Border />
+               <div className="grid grid-cols-3 py-6 place-items-center items-center justify-center gap-4">
+                  <Icons.Prada />
+                  <Icons.Burberry />
+                  <Icons.HugoBoss />
+                  <br />
+                  <Icons.Cartier />
+               </div>
+               <Icons.Border />
+            </section>
+            <section className="flex flex-col md:flex-row justify-center items-center gap-4 container mx-auto w-full mt-10">
+               <p className="text-titleActive uppercase title">Collections</p>
+               <div className="w-full relative">
+                  <Image
+                     src="/mobile/october.jpg"
+                     alt="10 october collections"
+                     className="absoloute left-0 top-0"
+                     width={0}
+                     height={0}
+                     sizes="100vw"
+                     unoptimized
+                     style={{
+                        width: "100%",
+                        height: "100%",
+                     }}
+                  />
+                  <div className={`absolute top-[20%] italic right-12`}>
+                     <p
+                        className={`text-body font-extrabold text-[160px] opacity-30 leading-none -tracking-[0.2em] ${bodoniModa.className}`}
+                     >
+                        10
+                     </p>
+                     <p
+                        className={`absolute text-offWhite text-[40px] left-[20px] top-[22%] ${bodoniModa.className}`}
+                     >
+                        October
+                     </p>
+                     <p className="uppercase body-s text-offWhite  left-[20px] absolute top-[55%] !tracking-[5px]">
+                        collection
+                     </p>
                   </div>
                </div>
-            </header>
-            <Image
-               src="/hero-image.jpg"
-               alt="spring season"
-               width={0}
-               height={0}
-               sizes="100vw"
-               style={{
-                  width: "100%",
-                  height: "100vh",
-                  objectFit: "cover",
-                  objectPosition: "bottom",
-               }}
-            />
-            <p className="font-bold text-neutral-500 absolute left-[50%] md:left-10 -translate-x-[50%] bottom-[75%] lg:translate-x-0 lg:left-20 lg:bottom-20 text-2xl">
-               Spring Season
-            </p>
-            <Link
-               className="absolute bottom-[70%] right-[50%] translate-x-[50%] md:translate-x-0 md:right-10 lg:bottom-20  bg-[#d26110] text-white-100 px-2 py-1"
-               href={"/products"}
-            >
-               View Collection
-            </Link>
-         </section>
-         <section className="flex justify-around items-center w-full py-10 bg-[#FC7A1D]">
-            <span className="hidden md:block">
-               <Icons.NumeroIcon />
-            </span>
-            <span className="hidden md:block">
-               <Icons.KinfolkIcon />
-            </span>
-            <Icons.FashionBeansIcon />
-            <Icons.HyperbeastLogo />
-         </section>
-         <section className="grid grid-cols-1 items-center justify-items-between md:grid-cols-2 container mx-auto">
-            <div className="flex justify-end items-center">
+            </section>
+            <section className="p-10 relative">
+               <Image
+                  src="/mobile/autumn_collection.jpg"
+                  alt="autumn collection"
+                  unoptimized
+                  sizes="100vw"
+                  width={292}
+                  height={260}
+               />
+               <p
+                  className={`right-[20%] top-[20%] text-body italic opacity-80 font-bold absolute ${bodoniModa.className} text-4xl`}
+               >
+                  Autumn
+               </p>
+               <p className="uppercase body-s text-body  right-[20%] top-[30%] absolute !tracking-[5px]">
+                  collection
+               </p>
+            </section>
+            <section className="flex flex-col md:flex-row flex-wrap items-center gap-4 text-center justify-center w-full pt-0 mx-auto container mt-10">
+               <h3 className="uppercase text-titleActive">Just for you</h3>
+               <Icons.Border />
+
                <Carousel
                   images={[
-                     "https://placehold.co/600x400/121112/FFFFFF.png",
-                     "https://placehold.co/600x400/123412/FFFFFF.png",
-                     "https://placehold.co/600x400/122222/FFFFFF.png",
+                     "https://placehold.co/254x311/000000/FFFFFF/png",
+                     "https://placehold.co/254x311/000000/FAFAFA/png",
+                     "https://placehold.co/254x311/000000/F3F3F3/png",
                   ]}
                />
-            </div>
-            <div className="relative max-w-[590px]">
-               <Image
-                  src={"/image-1.jpg"}
-                  alt="image"
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                  style={{
-                     width: "100%",
-                     height: "100%",
-                     objectFit: "cover",
-                     objectPosition: "center",
-                  }}
-               />
-               <Link
-                  className="absolute bottom-10 left-10 bg-neutral-900 text-white-100 text-sm px-4 py-2 rounded-md"
-                  href={"/products"}
-               >
-                  Shop shirt
-               </Link>
-            </div>
-         </section>
-         <section className="flex flex-col md:flex-row justify-center gap-4 container mx-auto w-full mt-10">
-            <div className="max-w-[596px] h-[600px] bg-white-200 rounded-md flex justify-end items-center relative">
-               <Image
-                  src={"/hero-image-2.png"}
-                  alt="image"
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                  style={{
-                     width: "100%",
-                     height: "100%",
-                     objectFit: "cover",
-                     objectPosition: "center",
-                  }}
-               />
-               <Link
-                  className="absolute bottom-10 left-10 bg-neutral-900 text-white-100 text-sm px-4 py-2 rounded-md"
-                  href={"/products"}
-               >
-                  Shop Sweaters
-               </Link>
-            </div>
-            <div className="bg-[#FC7A1D] p-10 max-w-[596px] h-[596px] text-neutral-900 flex flex-col justify-between rounded-lg w-full">
-               <p className="font-bold text-3xl">
-                  “ First Light promises <br /> quality, timeless designs <br />
-                  and with our mission it is <br /> truly a buy it for life
-                  purchase. “
-               </p>
-               <p>— Guera Latissa, Creative Director</p>
-            </div>
-         </section>
-         <section className="flex flex-col md:flex-row flex-wrap justify-center w-full pt-0 mx-auto container mt-10">
-            <div className="bg-['#FC7A1D'] relative h-[600px] lg:w-[586px] flex">
-               <Image
-                  src={"/womens.jpg"}
-                  alt="womens"
-                  sizes="100vw"
-                  width={0}
-                  height={0}
-                  className="lg:rounded-l-md"
-                  style={{
-                     width: "100%",
-                     height: "100%",
-                     objectFit: "cover",
-                     objectPosition: "center",
-                  }}
-               />
-               <Link
-                  href={"/products"}
-                  className="flex items-center gap-2 absolute left-8 bottom-8"
-               >
-                  WOMEN
-                  <Icons.ArrowTopRight />
-               </Link>
-            </div>
-            <div className="bg-['#FC7A1D'] relative  h-[600px] lg:w-[586px]">
-               <Image
-                  src={"/mens.jpg"}
-                  alt="mens"
-                  sizes="100vw"
-                  width={0}
-                  height={0}
-                  className="lg:rounded-r-md"
-                  style={{
-                     width: "100%",
-                     height: "100%",
-                     objectFit: "cover",
-                     objectPosition: "center",
-                  }}
-               />
-               <Link
-                  href={"/products"}
-                  className="flex items-center gap-2 absolute left-8 bottom-8"
-               >
-                  MEN
-                  <Icons.ArrowTopRight />
-               </Link>
-            </div>
-         </section>
-         <section className="flex flex-col md:flex-row w-full justify-center mx-auto container my-10 rounded-md">
-            <div className="lg:w-[580px]">
-               <Image
-                  src="/visit-our.jpg"
-                  alt="visit our popup shop"
-                  sizes="100vw"
-                  width={0}
-                  height={0}
-                  style={{
-                     width: "100%",
-                     height: "100%",
-                     objectFit: "cover",
-                  }}
-               />
-            </div>
-            <div className=" flex flex-col justify-between gap-8 p-16 font-semibold lg:w-[590px] bg-white-900">
-               <div className="flex flex-col gap-8">
-                  <h3>Visit our popup shop</h3>
-                  <p>
-                     Willemsparkweg 63 <br /> 1071 GS Amsterdam <br /> The
-                     Netherlands
-                  </p>
-               </div>
-               <div className="grid grid-cols-2 w-[308px] gap-6">
-                  <p>Monday</p>
-                  <p>12.00 - 19.00</p>
-                  <p>Tuesday</p>
-                  <p>10.00 - 19.00</p>
-                  <p>WednesDay</p>
-                  <p>10.00 - 19.00</p>
-                  <p>Thursday</p>
-                  <p>10.00 - 21.00</p>
-                  <p>Friday</p>
-                  <p>10.00 - 19.00</p>
-                  <p>Saturday</p>
-                  <p>10.00 - 18.00</p>
-                  <p>Sunday</p>
-                  <p>12.00 - 18.00</p>
-               </div>
-            </div>
-         </section>
+            </section>
+            <section className="flex flex-col md:flex-row w-full justify-center mx-auto container my-10 rounded-md"></section>
+         </main>
          <Footer />
-      </main>
+      </div>
    )
 }
