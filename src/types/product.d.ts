7a198a9d6da101885320d87abb8be6cb486f426d
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-13 00:33:37 +0330

    add product type

---
+++ src/types/product.d.ts
@@ -0,0 +1,27 @@
+type TProduct = {
+   attribute: {
+      favorite: false
+   }
+   brand: string | null
+   category: number
+   comments: []
+   created_at: string
+   description: ""
+   extra_information: string | null
+   featured: boolean
+   final_price: string
+   id: number
+   images: []
+   main_image: string
+   name: string
+   options: {
+      sizes: []
+      colors: []
+   }
+   price: string
+   rate: number
+   related_products: []
+   remaining: number
+   sell: number
+   slug: string
+}
