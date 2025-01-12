1a84ff1f6f4859b6f949b1d4bb07f4ec73334e45
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-12 12:24:08 +0330

    change style

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,35 @@
+import Footer from "@/components/Footer/Footer"
+import Header from "@/components/Header/Header"
+import { Icons } from "@/components/Icons/icons"
 import Link from "next/link"
 
 export default function NotFound() {
    return (
-      <div className="w-[100vw] h-[100vh] flex items-center gap-4 justify-center">
-         <p className="font-bold ">404</p>
-         <p>|</p>
-         <Link href={"/"} className="hover:underline transition">
-            HOME
-         </Link>
-      </div>
+      <>
+         <Header />
+         <div className="w-[100vw] h-[100vh] flex flex-col p-8 text-center items-center justify-around">
+            <p className="font-bold title">PAGE NOT FOUND </p>
+            <div className="flex flex-col items-center gap-10">
+               <p>
+                  <Icons.Dress />
+               </p>
+               <p
+                  style={{
+                     fontSize: "16px",
+                  }}
+               >
+                  We can't find the page you looking for, it will return to the
+               </p>
+
+               <Link
+                  href={"/"}
+                  className="hover:underline transition bg-black text-white-100 px-8 py-2 "
+               >
+                  HOME
+               </Link>
+            </div>
+         </div>
+         <Footer />
+      </>
    )
 }
