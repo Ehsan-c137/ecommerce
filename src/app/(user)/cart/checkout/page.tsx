b319b76480f9d749e1e50e58c18c0847e6870732
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-12 12:17:06 +0330

    add loader and checkout button

--- src/app/(user)/cart/checkout/page.tsx
+++ src/app/(user)/cart/checkout/page.tsx
@@ -1,87 +1,153 @@
 "use client"
 
-import z from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-
-const SigninSchema = z.object({
-   addresName: z
-      .string()
-      .min(3, { message: "title must be at least 3 characters long" }),
-   phone: z.number().min(1, { message: "price must bte at least 1" }),
-   name: z.string().min(1, { message: "category is required" }),
-   price: z.number(),
-})
-
-type TSigninSchema = z.infer<typeof SigninSchema>
+import { Icons } from "@/components/Icons/icons"
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query"
+import { getCart } from "@/services/store/cart/Cart"
+import { IPutCart, putCart } from "@/services/store/cart/Cart"
+import toast from "react-hot-toast"
+import Item from "../Item"
+import Link from "next/link"
+import clsx from "clsx"
+import style from "../cart.module.css"
 
 export default function Checkout() {
-   const {
-      register,
-      formState: { errors },
-   } = useForm<TSigninSchema>({
-      resolver: zodResolver(SigninSchema),
-      mode: "onChange",
+   const { data: cart, isLoading } = useQuery({
+      queryKey: ["cart"],
+      queryFn: () => getCart(),
    })
 
+   const mutation = useMutation({
+      mutationFn: (data: IPutCart[]) => putCart(data),
+      onError: (error) => {
+         console.log("error", error)
+         toast.error("something went wrong")
+      },
+   })
+
+   const queryClient = useQueryClient()
+
+   const handleCart = (
+      id: string,
+      count: number,
+      colors: string,
+      sizes: string
+   ) => {
+      const newDataCart = cart
+      const findIndex = newDataCart?.data?.findIndex(
+         (item: {
+            data: {
+               id: string
+            }
+            colors: string
+            sizes: string
+         }) =>
+            item.data.id == id && item.colors == colors && item.sizes == sizes
+      )
+      if (count === 0) {
+         newDataCart.data.splice(findIndex, 1)
+      } else {
+         newDataCart.data[findIndex].count = count
+      }
+
+      mutation.mutate(newDataCart.data)
+      queryClient.invalidateQueries({
+         queryKey: ["cart"],
+      })
+   }
+
+   const subtotal = cart?.data?.reduce(
+      (
+         acc: number,
+         item: { count: number; data: { price: number | string } }
+      ) => {
+         return item.count * Number(item.data?.price) + acc
+      },
+      0
+   )
+
    return (
-      <div className="flex flex-col gap-4">
-         <div className="flex flex-col">
-            <label
-               htmlFor="title"
-               className="text-neutral-600 font-medium text-sm"
-            >
-               name
-            </label>
-            <input
-               autoComplete="true"
-               {...register("name")}
-               type="text"
-               id="title"
-               className="border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
-            />
-            <p className="text-red-r500 mt-2 text-wrap">
-               {errors.name?.message}
-            </p>
+      <>
+         <div className="flex flex-col items-center gap-4">
+            <div className="w-full justify-around items-center flex">
+               <div className="justify-center flex-col items-center flex gap-1 w-full relative">
+                  <Icons.Border />
+               </div>
+            </div>
+            {isLoading ? (
+               <div className="w-full flex justify-center items-center min-h-[300px]">
+                  <span className="loader-black"></span>
+               </div>
+            ) : (
+               <div className="flex flex-col gap-2 px-4 h-full overflow-auto">
+                  {cart?.data?.map(
+                     (item: {
+                        data: {
+                           id: string
+                           main_image: string
+                           name: string
+                           price: number | string
+                           remaining: number
+                        }
+                        count: number
+                        colors: string
+                        sizes: string
+                     }) => (
+                        <Item
+                           key={item.colors + item.sizes}
+                           item={item}
+                           handleCart={handleCart}
+                        />
+                     )
+                  )}
+               </div>
+            )}
          </div>
-         <div className="flex flex-col">
-            <label
-               htmlFor="price"
-               className="text-neutral-600 font-medium text-sm"
+         {!isLoading && (
+            <div
+               className={clsx("fixed bottom-[56px] w-full px-4 opacity-0", {
+                  [style.show_info]: !isLoading,
+               })}
             >
-               phone number
-            </label>
-            <input
-               autoComplete="true"
-               {...register("price")}
-               type="number"
-               required
-               id="price"
-               className="border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
-            />
-            <p className="text-red-r500 mt-2 text-wrap">
-               {errors.price?.message}
-            </p>
-         </div>
-         <div className="flex flex-col">
-            <label
-               htmlFor="price"
-               className="text-neutral-600 font-medium text-sm"
-            >
-               Address name
-            </label>
-            <input
-               autoComplete="true"
-               {...register("addresName")}
-               type="number"
-               required
-               id="addresName"
-               className="border border-neutral-100 focus-within:border-neutral-900 px-4 py-2 outline-none transition-colors rounded-md"
-            />
-            <p className="text-red-r500 mt-2 text-wrap">
-               {errors.addresName?.message}
-            </p>
-         </div>
-      </div>
+               <div className="flex gap-2 justify-between py-2 px-1 border-t">
+                  <div className="flex gap-2">
+                     <Icons.Delivery />
+                     <p className="text-body body-m">Delivery</p>
+                  </div>
+                  <p className="text-label body-m">Free</p>
+               </div>
+               <div className="flex gap-2 py-2 border-t px-1">
+                  <Icons.Voucher />
+                  <p className="text-body body-m">Add promo code</p>
+               </div>
+               <div className="border-t flex justify-between gap-4 py-4 px-1">
+                  <p className="subtitle uppercase">EST. Total</p>
+                  <p className="text-secondary subtitle tracking-[3px]">
+                     ${subtotal}
+                  </p>
+               </div>
+            </div>
+         )}
+         <Link
+            href={"/cart/checkout"}
+            className={clsx(
+               "fixed -bottom-[60px] w-full text-offWhite flex justify-center items-center gap-6 bg-black h-[56px]",
+               {
+                  [style.show_button]: !isLoading,
+               }
+            )}
+         >
+            {isLoading ? (
+               <span className="loader"></span>
+            ) : (
+               <Link
+                  href="/cart/checkout/shipping"
+                  className="flex items-center gap-4"
+               >
+                  <Icons.ShoppingBagWhite />
+                  <p className="text-[18px] uppercase">checkout</p>
+               </Link>
+            )}
+         </Link>
+      </>
    )
 }
