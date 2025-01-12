69e6daa0f62cfb619a48f0e870f5abe1272f858b
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-12 13:08:15 +0330

    add content

--- src/app/(user)/products/[slug]/singleproduct/Details.tsx
+++ src/app/(user)/products/[slug]/singleproduct/Details.tsx
@@ -1,16 +1,40 @@
+import { Icons } from "@/components/Icons/icons"
+
 export default function Details() {
    return (
-      <>
-         <h5 className="font-semibold mb-4">Detail</h5>
-         <p className="text-neutral-500">
-            Elevate your everyday style with our Men&apos;s Black T-Shirts, the
-            ultimate wardrobe essential for modern men. Crafted with meticulous
-            attention to detail and designed for comfort, these versatile black
-            tees are a must-have addition to your collection. The classic black
-            color never goes out of style. Whether you&apos;re dressing up for a
-            special occasion or keeping it casual, these black t-shirts are the
-            perfect choice, effortlessly complementing any outfit.
-         </p>
-      </>
+      <div className="px-4 flex flex-col gap-6">
+         <div className="flex flex-col gap-2">
+            <p className="subtitle uppercase">Materials</p>
+            <p className="text-label">
+               We work with monitoring programmes to ensure compliance with
+               safety, health and quality standards for our products.
+            </p>
+         </div>
+         <div className="flex flex-col gap-2">
+            <p className="subtitle uppercase">Care</p>
+            <p className="text-label">
+               To keep your jackets and coats clean, you only need to freshen
+               them up and go over them with a cloth or a clothes brush. If you
+               need to dry clean a garment, look for a dry cleaner that uses
+               technologies that are respectful of the environment.
+            </p>
+         </div>
+         <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-2">
+               <Icons.DoNotBleach />
+               <p className="body-m text-label">Do not use bleach</p>
+            </div>
+            <div className="flex items-center gap-2">
+               <Icons.DoNotTumble />
+               <p className="body-m text-label">Do not tumble dry</p>
+            </div>
+            <div className="flex items-center gap-2">
+               <Icons.Iron />
+               <p className="body-m text-label">
+                  Iron at a maximum of 110ºC/230ºF
+               </p>
+            </div>
+         </div>
+      </div>
    )
 }
