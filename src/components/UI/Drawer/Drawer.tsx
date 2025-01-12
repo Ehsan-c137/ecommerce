88f09390ff8507ee825cc189b753a820f7e8d5bb
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-13 00:32:37 +0330

    delete extra imports

--- src/components/UI/Drawer/Drawer.tsx
+++ src/components/UI/Drawer/Drawer.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useCallback, useEffect, useRef, useState } from "react"
+import { useCallback, useEffect, useRef } from "react"
 import style from "./Drawer.module.css"
 import clsx from "clsx"
 import CreatePortal from "@/components/CreatePortal"
