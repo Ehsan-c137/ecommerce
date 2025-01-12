80483b7122ac3a028ea3095a3065612343248a61
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-12 12:22:34 +0330

    delete extra margin

--- src/app/(user)/products/page.tsx
+++ src/app/(user)/products/page.tsx
@@ -3,7 +3,7 @@ import Filters from "./Filters"
 export default function Page() {
    return (
       <>
-         <div className="flex gap-6 container mx-auto mt-4 px-4">
+         <div className="flex gap-6 container mx-auto px-4">
             <Filters />
          </div>
       </>
