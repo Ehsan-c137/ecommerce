7868122d76899a5a11089a2c18517f17fd9950ea
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-13 19:17:34 +0330

    change baseurl path, name

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,7 +3,7 @@ export const BASE_URL = "http://localhost:8000"
 import axios from "axios"
 
 export default axios.create({
-   baseURL: process.env.BASE_URL,
+   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
    headers: {
       "Content-Type": "application/json",
    },
--- src/services/user/signup.ts
+++ src/services/user/signup.ts
@@ -1,12 +1,12 @@
-"use server";
+"use server"
 
-import { BASE_URL } from "../index";
-import { cookies } from "next/headers";
+import { BASE_URL } from "../index"
+import { cookies } from "next/headers"
 
 export default async function signup(data: {
-   email: string;
-   password: string;
-   username: string;
+   email: string
+   password: string
+   username: string
 }) {
    const option = {
       method: "POST",
@@ -14,24 +14,27 @@ export default async function signup(data: {
          "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
-   };
+   }
 
    try {
-      const response = await fetch(`${BASE_URL}/user/register/`, option);
+      const response = await fetch(
+         `${process.env.NEXT_PUBLIC_BASE_URL}/user/register/`,
+         option
+      )
 
-      const data = await response.json();
+      const data = await response.json()
       if (response.status >= 200 && response.status < 300) {
          cookies().set({
             name: "session",
             value: data?.token,
             maxAge: 24 * 60 * 60 * 1000,
             httpOnly: true,
-         });
+         })
       }
 
-      console.log({ data, response });
-      return data;
+      console.log({ data, response })
+      return data
    } catch (error) {
-      return error;
+      return error
    }
 }
