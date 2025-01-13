ce613e68caaf06d59e43440b106a99c3720d7702
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-13 16:10:07 +0330

    add type

--- src/services/store/product/products.ts
+++ src/services/store/product/products.ts
@@ -1,12 +1,12 @@
-import api from "@/services/index";
+import api from "@/services/index"
 
-export default async function product(limit, page) {
+export default async function product(limit: number | string, page: number) {
    try {
       const response = await api.get(
          `/store/product?limit=${limit}&page=${page}`
-      );
-      return response.data;
+      )
+      return response.data
    } catch (error) {
-      return error;
+      return error
    }
 }
