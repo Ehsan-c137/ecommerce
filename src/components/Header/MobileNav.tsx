3c28b5e4b4744763154317fcd35e86fbb46ec8e5
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-15 17:58:04 +0330

    add shop cart

--- src/components/Header/MobileNav.tsx
+++ src/components/Header/MobileNav.tsx
@@ -1,10 +1,10 @@
 import HamburgerMenu from "@/components/UI/HamburgerMenu/HamburgerMenu"
 import { Icons } from "../Icons/icons"
-import checkLoggedin from "@/services/user/check_loggedin"
+// import checkLoggedin from "@/services/user/check_loggedin"
 import Link from "next/link"
 
 export default async function MobileNav() {
-   const isLoggedin = await checkLoggedin()
+   // const isLoggedin = await checkLoggedin()
 
    return (
       <header className="flex justify-between py-3 px-4">
@@ -16,11 +16,9 @@ export default async function MobileNav() {
          </Link>
          <div className="flex items-center gap-2">
             <Icons.Search />
-            {isLoggedin && (
-               <Link href={"/cart"}>
-                  <Icons.ShopCart />
-               </Link>
-            )}
+            <Link href={"/cart"}>
+               <Icons.ShopCart />
+            </Link>
          </div>
       </header>
    )
