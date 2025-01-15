b9271705f673c855786e3512bfc0b396288db67c
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-15 17:58:46 +0330

    add forwardRef

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -1,13 +1,28 @@
-export default function Input({ ...props }) {
+import { forwardRef, ForwardedRef } from "react"
+
+interface InputProps {
+   className?: string
+   [key: string]: unknown
+}
+
+const Input = forwardRef(function myInput(
+   props: InputProps,
+   ref: ForwardedRef<HTMLInputElement>
+) {
+   const { className, ...otherProps } = props
+
    return (
       <div>
          <input
+            ref={ref}
             style={{
                outline: "none",
             }}
-            {...props}
-            className={`flex text-[#979797] pb-1 px-2 border-b focus-within:border-[black] transition-colors bg-transparent w-full ${props.className}`}
+            {...otherProps}
+            className={`flex text-[#979797] pb-1 px-2 border-b focus-within:border-[black] transition-colors bg-transparent w-full ${className}`}
          />
       </div>
    )
-}
+})
+
+export default Input
