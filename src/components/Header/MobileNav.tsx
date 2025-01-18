a5f3130831f6adb0706778ae8e72c11ad830b940
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-18 22:08:38 +0330

    add search component

--- src/components/Header/MobileNav.tsx
+++ src/components/Header/MobileNav.tsx
@@ -2,23 +2,26 @@ import HamburgerMenu from "@/components/UI/HamburgerMenu/HamburgerMenu"
 import { Icons } from "../Icons/icons"
 import checkLoggedin from "@/services/user/check_loggedin"
 import Link from "next/link"
+import Search from "@/components/UI/Search"
 
 export default async function MobileNav() {
    const isLoggedin = await checkLoggedin()
    return (
-      <header className="flex justify-between py-3 px-4">
-         <div className="w-[56px] flex items-center">
-            <HamburgerMenu isLoggedIn={isLoggedin} />
-         </div>
-         <Link href={"/"}>
-            <Icons.Logo />
-         </Link>
-         <div className="flex items-center gap-2">
-            <Icons.Search />
-            <Link href={"/cart"}>
-               <Icons.ShopCart />
+      <>
+         <header className="flex justify-between py-3 px-4">
+            <div className="w-[56px] flex items-center">
+               <HamburgerMenu isLoggedIn={isLoggedin} />
+            </div>
+            <Link href={"/"}>
+               <Icons.Logo />
             </Link>
-         </div>
-      </header>
+            <div className="flex items-center gap-2">
+               <Search />
+               <Link href={"/cart"}>
+                  <Icons.ShopCart />
+               </Link>
+            </div>
+         </header>
+      </>
    )
 }
