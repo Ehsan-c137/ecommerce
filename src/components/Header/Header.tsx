742f2e4333f61260048dc58c070c4f57f49a0745
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-19 18:53:46 +0330

    delete extra div

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,9 +9,8 @@ export default async function Header() {
 
    return (
       <>
-         <div>
-            <MobileNav />
-         </div>
+         <MobileNav />
+
          <header className="hidden lg:fixed w-full h-21 top-0 z-10 bg-white-900 px-4">
             <div className="flex items-center justify-between h-[84px] 3xl:container mx-auto">
                <div className="flex items-center gap-4 lg:w-[337px]">
