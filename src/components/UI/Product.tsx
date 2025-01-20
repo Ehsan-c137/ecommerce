8e744cb76323827f8ab6556ec77367760ec7dc43
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-20 14:33:16 +0330

    add optional chaining

--- src/components/UI/Product.tsx
+++ src/components/UI/Product.tsx
@@ -1,15 +1,22 @@
 import Image from "next/image"
+import { useState } from "react"
 export default function Product({ item }) {
+   const [isLoaded, setIsLoaded] = useState(false)
    return (
       <div className={`w-[165px] text-center`}>
          <Image
-            src={item.main_image}
+            src={item?.main_image}
             width={"165"}
             height={200}
-            alt={item.name}
+            alt={item?.name}
+            className="transition"
+            style={{
+               opacity: isLoaded ? 1 : 0,
+            }}
+            onLoad={() => setIsLoaded(true)}
          />
-         <p className="text-body body-s">{item.name}</p>
-         <p className="text-primary text-md">${item.price}</p>
+         <p className="text-body body-s">{item?.name}</p>
+         <p className="text-primary text-md">${item?.price}</p>
       </div>
    )
 }
