2291e6f666c7eb04cc4ea1c0f732c9f034470779
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-20 16:31:17 +0330

    add height to wrapper

--- src/components/UI/Product.tsx
+++ src/components/UI/Product.tsx
@@ -4,15 +4,16 @@ export default function Product({ item }: { item: TProduct }) {
    const [isLoaded, setIsLoaded] = useState(false)
    return (
       <div className={`w-[165px] text-center`}>
-         <div>
+         <div className="h-[220px]">
             <Image
                src={item?.main_image}
                width={"165"}
-               height={200}
+               height={220}
                alt={item?.name}
                className="transition"
                style={{
                   opacity: isLoaded ? 1 : 0,
+                  objectFit: "cover",
                }}
                onLoad={() => setIsLoaded(true)}
             />
