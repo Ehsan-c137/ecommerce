c76e285610f69c5ea5bd86fdefb44a6404ad1e3f
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-20 14:25:54 +0330

    add prop

--- src/components/UI/Product.tsx
+++ src/components/UI/Product.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image"
-export default function Product() {
+export default function Product({ item }) {
    return (
       <div className={`w-[165px] text-center`}>
-         <Image src={"/image-p.png"} width={"165"} height={200} alt="image" />
-         <p className="text-body body-s">21WN reversible angora cardigan</p>
-         <p className="text-primary text-md">$120</p>
+         <Image
+            src={item.main_image}
+            width={"165"}
+            height={200}
+            alt={item.name}
+         />
+         <p className="text-body body-s">{item.name}</p>
+         <p className="text-primary text-md">${item.price}</p>
       </div>
    )
 }
