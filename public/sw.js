529dfc342f5c6f5a83ee01b670b139dda88e69d2
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-20 23:59:54 +0330

    add service worker

--- public/sw.js
+++ public/sw.js
@@ -3,8 +3,8 @@ self.addEventListener("push", function (event) {
       const data = event.data.json()
       const options = {
          body: data.body,
-         icon: data.icon || "/icon.png",
-         badge: "/badge.png",
+         icon: data.icon || "/android-chrome-512x512.png",
+         badge: "/android-chrome-512x512.png",
          vibrate: [100, 50, 100],
          data: {
             dateOfArrival: Date.now(),
