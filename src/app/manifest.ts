bddfbdf6f0411e70e9896fe1f41151e89123e68b
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-20 18:08:04 +0330

    update description

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -4,7 +4,7 @@ export default function manifest(): MetadataRoute.Manifest {
    return {
       name: "Open Fashion",
       short_name: "OpenFashion",
-      description: "A Progressive Web App built with Next.js",
+      description: "a place for find your style",
       start_url: "/",
       display: "standalone",
       background_color: "#ffffff",
