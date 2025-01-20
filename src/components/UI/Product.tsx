b33f68cd0d4cebec65b681ce3e11317cf738750c
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-20 16:27:11 +0330

    add wrapper for image

--- src/components/UI/Product.tsx
+++ src/components/UI/Product.tsx
@@ -4,17 +4,19 @@ export default function Product({ item }: { item: TProduct }) {
    const [isLoaded, setIsLoaded] = useState(false)
    return (
       <div className={`w-[165px] text-center`}>
-         <Image
-            src={item?.main_image}
-            width={"165"}
-            height={200}
-            alt={item?.name}
-            className="transition"
-            style={{
-               opacity: isLoaded ? 1 : 0,
-            }}
-            onLoad={() => setIsLoaded(true)}
-         />
+         <div>
+            <Image
+               src={item?.main_image}
+               width={"165"}
+               height={200}
+               alt={item?.name}
+               className="transition"
+               style={{
+                  opacity: isLoaded ? 1 : 0,
+               }}
+               onLoad={() => setIsLoaded(true)}
+            />
+         </div>
          <p className="text-body body-s truncate">{item?.name}</p>
          <p className="text-primary text-md">${item?.price}</p>
       </div>
