82ca343ec50985d9cc6e26d54bcf8c7c6706a707
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-20 22:52:04 +0330

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next"
+import type { Metadata, Viewport } from "next"
 import { Tenor_Sans } from "next/font/google"
 import "./globals.css"
 import Providers from "@/providers"
@@ -6,6 +6,23 @@ import Providers from "@/providers"
 export const metadata: Metadata = {
    title: "Open Fashion",
    description: "a place for everything you want",
+   icons: [
+      {
+         url: "/android-chrome-192x192.png",
+         sizes: "192x192",
+         type: "image/png",
+      },
+      {
+         url: "/android-chrome-512x512.png",
+         sizes: "512x512",
+         type: "image/png",
+      },
+   ],
+}
+
+export const viewport: Viewport = {
+   maximumScale: 1,
+   userScalable: false,
 }
 
 const tenor_sans = Tenor_Sans({ subsets: ["latin"], weight: "400" })
