53f8a732efee37d74a95bc28a22724f7ff1e474e
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-20 21:44:06 +0330

    update image path

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,13 +11,13 @@ export default function manifest(): MetadataRoute.Manifest {
       theme_color: "#000000",
       icons: [
          {
-            src: "/favicon.ico/web-app-manifest-192x192.png",
+            src: "/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "maskable",
          },
          {
-            src: "/favicon.ico/web-app-manifest-512x512.png",
+            src: "/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
