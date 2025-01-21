4c52ac91fdd0616ee022c4e9e99c1625133a5750
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-21 20:31:36 +0330

    handle errors

--- src/services/store/product/singleProduct.ts
+++ src/services/store/product/singleProduct.ts
@@ -1,10 +1,15 @@
-import api from "@/services/index";
+import api from "@/services/index"
+import { AxiosError } from "axios"
 
 export default async function getSingleProduct(slug: string) {
    try {
-      const response = await api.get(`/store/product/slug/${slug}`);
-      return response.data;
+      const response = await api.get(`/store/product/slug/${slug}`)
+
+      return response.data
    } catch (error) {
-      console.log(error);
+      if (error instanceof AxiosError) {
+         return error.response
+      }
+      return error
    }
 }
