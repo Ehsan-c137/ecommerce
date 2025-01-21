b2efde24c32b3e54ee775838f87751fda7fda524
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-21 20:31:12 +0330

    delete header

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,10 @@
 import Footer from "@/components/Footer/Footer"
-import Header from "@/components/Header/Header"
 import { Icons } from "@/components/Icons/icons"
 import NotFoundButton from "@/components/NotFound"
 
 export default function NotFound() {
    return (
       <>
-         <Header />
          <div className="w-[100vw] h-[100vh] flex flex-col p-8 text-center items-center justify-around">
             <p className="font-bold title">PAGE NOT FOUND </p>
             <div className="flex flex-col items-center gap-10">
@@ -18,8 +16,7 @@ export default function NotFound() {
                      fontSize: "16px",
                   }}
                >
-                  We can&apos;t find the page you looking for, it will return to
-                  the
+                  We can&apos;t find the page you looking for
                </p>
 
                <NotFoundButton />
