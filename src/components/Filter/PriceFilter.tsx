9852b6ef649d559789c384b803af29043bcc9f0b
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-25 17:54:00 +0330

    add price handler prop

--- src/components/Filter/PriceFilter.tsx
+++ src/components/Filter/PriceFilter.tsx
@@ -5,16 +5,9 @@ export function PriceFilter({
    minPrice,
    maxPrice,
    isLoading,
-   onSelect,
+   handleSetMinPrice,
+   handleSetMaxPrice,
 }: PriceFilterProps) {
-   const handleSetMinPrice = (min: number) => {
-      onSelect("min price", min > minPrice ? `${min}` : "")
-   }
-
-   const handleSetMaxPrice = (max: number) => {
-      onSelect("max price", max <= maxPrice ? `${max}` : "")
-   }
-
    return (
       <div className="flex flex-col gap-4 px-4">
          <p className="text-neutral-900 font-medium">Price</p>
