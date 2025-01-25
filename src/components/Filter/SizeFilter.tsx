23dd615ff99ed196f60c78072c02d677590433b9
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-25 17:31:27 +0330

    add loading state

--- src/components/Filter/SizeFilter.tsx
+++ src/components/Filter/SizeFilter.tsx
@@ -2,38 +2,48 @@ import { FilterProps } from "@/types/types"
 
 interface SizeFilterProps extends FilterProps {
    sizes: string[]
+   isLoading: boolean
 }
 
-export function SizeFilter({ sizes, searchParams, onSelect }: SizeFilterProps) {
+export function SizeFilter({
+   sizes,
+   searchParams,
+   onSelect,
+   isLoading,
+}: SizeFilterProps) {
    return (
       <div className="flex flex-col gap-4 px-4">
          <p className="text-neutral-900 font-medium">Size</p>
-         <div className="flex items-center flex-wrap gap-3 text-sm">
-            {sizes?.map((size) => {
-               const isChecked = searchParams.getAll("size")?.includes(size)
-               return (
-                  <div key={size} className="flex items-center">
-                     <label
-                        style={{
-                           borderColor: isChecked ? "black" : "#e6e7e8",
-                        }}
-                        htmlFor={`${size}--size`}
-                        className="flex items-center justify-center w-10 h-10 rounded-md cursor-pointer border border-neutral-100 transition"
-                     >
-                        {size}
-                     </label>
-                     <input
-                        type="checkbox"
-                        value={size}
-                        id={`${size}--size`}
-                        className="hidden"
-                        checked={isChecked}
-                        onChange={(e) => onSelect("size", e.target.value)}
-                     />
-                  </div>
-               )
-            })}
-         </div>
+         {isLoading ? (
+            <div className="h-58px animate-pulse bg-white-200 rounded-sm"></div>
+         ) : (
+            <div className="flex items-center flex-wrap gap-3 text-sm">
+               {sizes?.map((size) => {
+                  const isChecked = searchParams.getAll("size")?.includes(size)
+                  return (
+                     <div key={size} className="flex items-center">
+                        <label
+                           style={{
+                              borderColor: isChecked ? "black" : "#e6e7e8",
+                           }}
+                           htmlFor={`${size}--size`}
+                           className="flex items-center justify-center w-10 h-10 rounded-md cursor-pointer border border-neutral-100 transition"
+                        >
+                           {size}
+                        </label>
+                        <input
+                           type="checkbox"
+                           value={size}
+                           id={`${size}--size`}
+                           className="hidden"
+                           checked={isChecked}
+                           onChange={(e) => onSelect("size", e.target.value)}
+                        />
+                     </div>
+                  )
+               })}
+            </div>
+         )}
       </div>
    )
 }
