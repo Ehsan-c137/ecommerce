e5f38901e82090c41205ca1ef65049783db9462d
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-25 13:18:39 +0330

    add colors

--- src/utils/toPastelColor.ts
+++ src/utils/toPastelColor.ts
@@ -13,7 +13,7 @@ function rgbToHex(r: number, g: number, b: number) {
    return `#${((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1)}`
 }
 
-function lightenColor(r: number, g: number, b: number, factor = 0.9) {
+function lightenColor(r: number, g: number, b: number, factor = 0.1) {
    return {
       r: Math.round(r + (255 - r) * factor),
       g: Math.round(g + (255 - g) * factor),
@@ -21,7 +21,7 @@ function lightenColor(r: number, g: number, b: number, factor = 0.9) {
    }
 }
 
-function desaturateColor(r: number, g: number, b: number, factor = 0.5) {
+function desaturateColor(r: number, g: number, b: number, factor = 0.4) {
    const gray = r * 0.3 + g * 0.59 + b * 0.11
    return {
       r: Math.round(r + (gray - r) * factor),
@@ -32,20 +32,29 @@ function desaturateColor(r: number, g: number, b: number, factor = 0.5) {
 
 function toPastel(color: string) {
    const skipColor = ["black", "blue"]
-   if (skipColor.includes(color.toLowerCase())) return color
+   if (skipColor.includes(color?.toLowerCase())) return color
    const colorMap: Record<string, string> = {
       black: "#000000",
       blue: "#0000FF",
+      red: "#FF0000",
+      green: "#00FF00",
+      yellow: "#FFFF00",
+      purple: "#800080",
+      orange: "#FFA500",
+      pink: "#FFC0CB",
+      brown: "#A52A2A",
+      gray: "#808080",
+      white: "#FFFFFF",
    }
 
-   const hex = colorMap[color.toLowerCase()] || color
+   const hex = colorMap[color?.toLowerCase()] || color
    const rgb = hexToRgb(hex)
-
+   console.log(rgb)
    if (!rgb) return color
 
    const lightened = lightenColor(rgb.r, rgb.g, rgb.b)
    const pastel = desaturateColor(lightened.r, lightened.g, lightened.b)
-
+   console.log(color, rgbToHex(pastel.r, pastel.g, pastel.b))
    return rgbToHex(pastel.r, pastel.g, pastel.b)
 }
 
