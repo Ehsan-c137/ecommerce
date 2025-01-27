9f35edebbd3f1578f0957fe81059801cf70abd6e
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-27 21:39:45 +0330

    add cols for md

--- src/components/LandingPage/NewArrivel.tsx
+++ src/components/LandingPage/NewArrivel.tsx
@@ -38,7 +38,7 @@ export default function NewArrival() {
                </button>
             ))}
          </div>
-         <div className="grid grid-cols-2 gap-4">
+         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
             {isLoading &&
                Array.from({ length: 4 }).map((_, i) => (
                   <div
