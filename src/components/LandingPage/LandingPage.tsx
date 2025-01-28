9f09554da1a820c914fac3e47836b24f66e729ae
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-01-28 19:59:49 +0330

    reduce landing animation dely

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -65,7 +65,7 @@ export default function LandingPage() {
          duration: 1.8,
          y: 100,
          ease: "power4.out",
-         delay: 1,
+         delay: 0.5,
          skewY: 7,
          stagger: {
             amount: 0.3,
