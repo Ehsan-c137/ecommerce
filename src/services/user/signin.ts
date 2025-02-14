2d1eeadb3719b060a974a568cf1c75cf65a795df
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-02-14 21:22:33 +0330

    add sameSite property

--- src/services/user/signin.ts
+++ src/services/user/signin.ts
@@ -1,15 +1,15 @@
-"use server";
+"use server"
 
-import api from "@/services/index";
-import { AxiosError } from "axios";
-import { cookies } from "next/headers";
+import api from "@/services/index"
+import { AxiosError } from "axios"
+import { cookies } from "next/headers"
 
 export default async function signup(data: {
-   username: string;
-   password: string;
+   username: string
+   password: string
 }) {
    try {
-      const response = await api.post("/user/login/", data);
+      const response = await api.post("/user/login/", data)
 
       if (response.status >= 200 && response.status < 300) {
          cookies().set({
@@ -17,14 +17,15 @@ export default async function signup(data: {
             value: response.data?.token,
             maxAge: 24 * 60 * 60 * 1000,
             httpOnly: true,
-         });
+            sameSite: true,
+         })
       }
 
-      return response.data;
+      return response.data
    } catch (error) {
       if (error instanceof AxiosError) {
-         return error.response?.data;
+         return error.response?.data
       }
-      return error;
+      return error
    }
 }
