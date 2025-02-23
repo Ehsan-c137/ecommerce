7ca45e26b333fb3f3bf31f8fa009d6701496a561
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-02-23 10:13:49 +0330

    add label for loading state

--- src/components/Filter/SizeFilter.tsx
+++ src/components/Filter/SizeFilter.tsx
@@ -15,7 +15,10 @@ export function SizeFilter({
       <div className="flex flex-col gap-2 px-4">
          <p className="text-neutral-900 font-medium">Size</p>
          {isLoading ? (
-            <div className="h-[58px] w-full animate-pulse bg-white-200 rounded-sm"></div>
+            <div
+               aria-label="size filter loading"
+               className="h-[58px] w-full animate-pulse bg-white-200 rounded-sm"
+            ></div>
          ) : (
             <div className="flex items-center flex-wrap gap-3 text-sm">
                {sizes?.map((size) => {
