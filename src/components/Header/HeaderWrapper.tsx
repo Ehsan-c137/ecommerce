855d5bf6a1a59054e9b362e8c2c26cb716bfd5cd
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2025-04-27 21:29:05 +0330

    delete logedin check

--- src/components/Header/HeaderWrapper.tsx
+++ src/components/Header/HeaderWrapper.tsx
@@ -1,9 +1,5 @@
-"use server"
-
 import Header from "./Header"
-import checkLoggedin from "@/services/user/check_loggedin"
 
 export default async function HeaderWrapper() {
-   const { isLoggedIn } = await checkLoggedin()
-   return <Header isLogged={isLoggedIn} />
+   return <Header />
 }
