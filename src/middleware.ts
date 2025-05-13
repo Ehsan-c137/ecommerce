300595b9d17e49fb49199510e2dd67ee8feb1d8d
Ehsan <ehsanmousavi1.79@gmail.com>
2025-05-13 20:20:29 +0330

    change path

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,18 @@
 import { NextResponse, NextRequest } from "next/server"
 
-const protectedRoute = ["/profile", "/protected"]
+const protectedRoute = ["/profile", "/dashboard"]
 
 export function middleware(request: NextRequest) {
-   const session = request.cookies.get("session")
-
+   const { pathname } = request.nextUrl
+   const sessionCookie = request.cookies.get("session")
+   console.log("hello ")
    const isProtectedRoute = protectedRoute.some((route) =>
-      request.nextUrl.pathname.startsWith(route)
+      pathname.startsWith(route)
    )
+   console.log(isProtectedRoute)
 
-   if (isProtectedRoute && !session) {
-      const currentPath = request.nextUrl.pathname + request.nextUrl.search
+   if (isProtectedRoute && !sessionCookie?.value) {
+      const currentPath = pathname + request.nextUrl.search
 
       const isValidCallback =
          currentPath.startsWith("/") &&
@@ -29,5 +31,10 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-   matcher: ["/profile/:path*", "/protected/:path*"],
+   matcher: [
+      "/profile",
+      "/profile/:path*",
+      "/dashboard/:path",
+      "/((?!api|_next/static|_next/image|favicon.ico).*)",
+   ],
 }
