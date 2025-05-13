f2ab39c757a36d980941e56e3293733e4a0fefef
Ehsan <ehsanmousavi1.79@gmail.com>
2025-05-13 20:20:56 +0330

    fix styles

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,8 @@
 export default function Loading() {
    return (
-      <div className="h-[100vh] w-[100vw]">
+      <div className="h-[100vh] w-[100vw] flex items-center justify-center">
          <p className="sr-only">Loading...</p>
+         <div className="loader h-[100vh] w-[100vw]"></div>
       </div>
    )
 }
